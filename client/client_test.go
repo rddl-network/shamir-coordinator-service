2bb275dfec1983786c150f0a55a976e8f25520a3
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-03-18 13:30:17 +0100

    test: fix test assertion
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- client/client_test.go
+++ client/client_test.go
@@ -13,6 +13,7 @@ import (
 
 func TestGetMnemonics(t *testing.T) {
 	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/mnemonics", r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"mnemonics":["word1","word2"]}`))
 	}))
