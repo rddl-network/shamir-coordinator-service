8a9497413ae21a210dcda37851e1448684c198fb
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-07-01 23:57:56 +0200

    feat: add IssueNFTAsset to elements_client.go
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- types/types.go
+++ types/types.go
@@ -23,3 +23,16 @@ type MnemonicsResponse struct {
 	Mnemonics []string `binding:"required" json:"mnemonics"`
 	Seed      string   `binding:"required" json:"seed"`
 }
+
+type Entity struct {
+	Domain string `json:"domain"`
+}
+
+type Contract struct {
+	Entity       Entity `json:"entity"`
+	IssuerPubkey string `json:"issuer_pubkey"` //nolint:tagliatelle // the format liquid network needs it
+	MachineAddr  string `json:"machine_addr"`  //nolint:tagliatelle // the format liquid network needs it
+	Name         string `json:"name"`
+	Precision    uint64 `json:"precision"`
+	Version      uint64 `json:"version"`
+}
